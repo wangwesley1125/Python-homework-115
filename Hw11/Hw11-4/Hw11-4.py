8b85c2ab02c479a2f11d3375d8cd03240786d9e0
wangwesley <wangwesley1125@gmail.com>
2026-06-07 19:58:38 +0800

    Finish Hw11-4

---
+++ Hw11/Hw11-4/Hw11-4.py
@@ -0,0 +1,9 @@
+print('{:c}{:c}{:c}'.format(58, 45, 41))
+
+print(end="\n")
+
+print('[{0:>10}]\n[{0:^10}]\n[{0:<10}]'.format('Amanda'))
+
+print(end="\n")
+
+print('{:+10,.2f}\n{:+10,.2f}'.format(10240.473, -3210.9521))
