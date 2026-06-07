5d13495307cb60ddbebfa50c65508fa36053757c
wangwesley <wangwesley1125@gmail.com>
2026-06-07 20:53:35 +0800

    Finish Hw12-5

---
+++ Hw12/Hw12-5/Hw12-5.py
@@ -0,0 +1,15 @@
+def try_it(value):
+    try:
+        x = int(value)
+
+    except ValueError:
+        print(f'{value} could not be converted to an integer')
+    
+    else:
+        print(f'int({value}) is {x}')
+    
+    finally:
+        print('finally executed')
+
+try_it(10.7)
+try_it('Python')
